0dd8ac4ae98d09344cedc91f4d3adcd054a57737
Tural <166192406+Tural-Abbasov@users.noreply.github.com>
2024-07-29 08:01:37 +0400

    Add files via upload

---
+++ Javascript Task 3/assets/js/script.js
@@ -0,0 +1,115 @@
+
+// Dom one 
+
+
+
+// function calculate(x) {
+//     let result=0;
+//     let valueOne=+ document.querySelectorAll ("input") [0].value
+//     let valueTwo=+ document.querySelectorAll ("input") [1].value
+//     if (x.innerText== "+") {
+//         result= valueOne+ valueTwo
+//     }else if (x.innerText== "-") {
+//         result= valueOne- valueTwo
+//     }else if (x.innerText== "*") {
+//         result= valueOne* valueTwo
+//     }else if (x.innerText== "/") {
+//         result= valueOne/ valueTwo
+//     }
+//     document.querySelector("span") .innerHTML=result
+// }
+
+
+
+// Dom Two 
+
+
+// function reverseText() {
+//     let inputText = document.getElementById("inputThree").value;
+//     let reversedText = inputText.split('').reverse().join('');
+//     document.getElementById("textTwo").innerText = "Ters soz: " + reversedText;
+// }
+
+
+
+
+// Dom three 
+
+
+
+// function changeDiv() {
+//     var divOne = document.getElementById("divOne");
+//     var inputWidth = document.getElementById("inputWidth");
+//     var inputHeight = document.getElementById("inputHeight");
+//     var inputBorderWidth = document.getElementById("inputBorderWidth");
+//     var selectBorderStyle = document.getElementById("selectBorderStyle");
+//     var selectBorderColor = document.getElementById("selectBorderColor");
+//     var selectColor = document.getElementById("selectColor");
+//     var selectBackground = document.getElementById("selectBackground");
+
+//     divOne.style.width = inputWidth.value + "px";
+//     divOne.style.height = inputHeight.value + "px";
+//     divOne.style.borderWidth = inputBorderWidth.value + "px";
+//     divOne.style.borderStyle = selectBorderStyle.value;
+//     divOne.style.borderColor = selectBorderColor.value;
+//     divOne.style.color = selectColor.value;
+//     divOne.style.backgroundColor = selectBackground.value;
+// }
+
+
+
+// Dom Four 
+
+
+
+// function changeDiv(){
+//     console.log(divTwo.style.width);
+//     divTwo.style.width=parseInt(divTwo.style.width)+(+inputWidth.value)+"px";
+//     divTwo.style.height=inputHeight.value+"px";
+//     divTwo.style.borderWidth=inputBorderWidth.value+"px"
+// }
+
+
+
+// Dom five 
+
+
+
+// function buttonCalc(x) {
+// displayInput.value+=x.innerText
+// }
+
+// function equal() {
+//     let result=0
+//     if(displayInput.value.includes("+")){
+//         let deyer=displayInput.value.split("+")
+//         console.log(deyer);
+//         for (let i = 0; i <deyer.length; i++){
+//             result+=(+deyer[i])
+//         }
+//     }else if(displayInput.value.includes("-")){
+//         let deyer=displayInput.value.split("-")
+//         result=(+deyer[0])
+//         for (let i = 1; i <deyer.length; i++){
+//             result-=(deyer[i])
+//         }
+//     }else if(displayInput.value.includes("/")){
+//         let deyer=displayInput.value.split("/")
+//         result=(+deyer[0])
+//         for (let i = 1; i <deyer.length; i++){
+//             result/=(deyer[i])
+//         }
+//     }else if(displayInput.value.includes("*")){
+//         let deyer=displayInput.value.split("*")
+//         result=(+deyer[0])
+//         for (let i = 1; i <deyer.length; i++){
+//             result*=(deyer[i])
+//         }
+//     }
+//     displayInput.value=result
+// }
+// function sil(){
+//     displayInput.value=""
+// }
+
+
